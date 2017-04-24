DashBlocks/scratch-blocks

commit 1ca48adbe4ec17ea3c2bdcb367d73f81decd610b

fixed when block palette scroll up a little (#868)
every time the workspace is rerendered.

core/flyout_vertical.js
@@ -208,7 +208,7 @@ Blockly.VerticalFlyout.prototype.getMetrics_ = function() {
     viewWidth: viewWidth,
     contentHeight: optionBox.height * this.workspace_.scale + 2 * this.MARGIN,
     contentWidth: optionBox.width * this.workspace_.scale + 2 * this.MARGIN,
-    viewTop: -this.workspace_.scrollY,
+    viewTop: -this.workspace_.scrollY + optionBox.y,
     viewLeft: -this.workspace_.scrollX,
     contentTop: optionBox.y,
     contentLeft: optionBox.x,

commit c205fcd9c3cdf82906806aec03c55832896dfe67

fixes dragging failure in blocks palette
as said in issue #873, which in introduced by commit 1ca48ad.

The change is using workspace.scroll instead of viewTop to calculate the position of the scrollbar.
Because the top of the view and the scrollbar position are two different things.

core/flyout_vertical.js
@@ -343,7 +343,7 @@ Blockly.VerticalFlyout.prototype.wheel_ = function(e) {
       delta *= 10;
     }
     var metrics = this.getMetrics_();
-    var pos = metrics.viewTop + delta;
+    var pos = -this.workspace_.scrollY + delta;
     var limit = metrics.contentHeight - metrics.viewHeight;
     pos = Math.min(pos, limit);
     pos = Math.max(pos, 0);
@@ -581,7 +581,7 @@ Blockly.VerticalFlyout.prototype.onMouseMove_ = function(e) {
   }
   var dy = e.clientY - this.startDragMouseY_;
   this.startDragMouseY_ = e.clientY;
-  var y = metrics.viewTop - dy;
+  var y = -this.workspace_.scrollY - dy;
   y = goog.math.clamp(y, 0, metrics.contentHeight - metrics.viewHeight);
   this.scrollbar_.set(y);
 };