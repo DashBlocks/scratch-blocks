94f0920a644f0f99438467b207226a023b5fc0e3
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2025-09-23 23:56:39 +0500

    Lil fixes

--- core/workspace_svg.js
+++ core/workspace_svg.js
@@ -1105,14 +1105,14 @@ Blockly.WorkspaceSvg.prototype.reportValue = function(id, value) {
   var maxShownItems = 50;
   if (Array.isArray(value)) {
     var more = value.length - maxShownItems;
-    var result = value.length === 0 ? '[]' : value.slice(0, maxShownItems).reduce(function(acc, value, i) {
+    var result = value.length === 0 ? '[]' : value.slice(0, maxShownItems).reduce(function(acc, value, i, array) {
       acc += i === 0 ? '[' : '';
       acc += Array.isArray(value)
         ? 'nested array'
         : typeof value === 'object' && value !== null
           ? 'nested object'
           : JSON.stringify(value);
-      acc += i === maxShownItems - 1 ? (more > 0 ? ', *' + more + ' more items*' : '') + ']' : ', ';
+      acc += i === array.length - 1 ? (more > 0 ? ', *' + more + ' more items*' : '') + ']' : ', ';
       return acc;
     }, '');
     valueReportBox.textContent = result;
@@ -1124,7 +1124,7 @@ Blockly.WorkspaceSvg.prototype.reportValue = function(id, value) {
       valueReportBox.appendChild(value.toReporterContent());
     } else {
       var more = Object.keys(value).length - maxShownItems;
-      var result = Object.keys(value).length === 0 ? '{}' : Object.entries(value).slice(0, 50).reduce(function(acc, value, i) {
+      var result = Object.keys(value).length === 0 ? '{}' : Object.entries(value).slice(0, 50).reduce(function(acc, value, i, array) {
         acc += i === 0 ? '{' : '';
         acc += JSON.stringify(value[0]) + ': ';
         acc += Array.isArray(value[1])
@@ -1132,7 +1132,7 @@ Blockly.WorkspaceSvg.prototype.reportValue = function(id, value) {
           : typeof value[1] === 'object' && value[1] !== null
             ? 'nested object'
             : JSON.stringify(value[1]);
-        acc += i === maxShownItems - 1 ? (more > 0 ? ', *' + more + ' more items*' : '') + '}' : ', ';
+        acc += i === array.length - 1 ? (more > 0 ? ', *' + more + ' more items*' : '') + '}' : ', ';
         return acc;
       }, '');
       valueReportBox.textContent = result;
