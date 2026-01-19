739628122582515739d2d96e44c85d1b28cba230
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2026-01-19 21:53:26 +0500

    Update console.js

--- blocks_vertical/console.js
+++ blocks_vertical/console.js
@@ -61,6 +61,26 @@ Blockly.Blocks['console_addline'] = {
   }
 };
 
+Blockly.Blocks['console_addlineandmove'] = {
+  /**
+   * Block to add line in console and move cursor.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": Blockly.Msg.CONSOLE_ADDLINEANDMOVE,
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "LINE"
+        }
+      ],
+      "category": Blockly.Categories.console,
+      "extensions": ["colours_console", "shape_statement"]
+    });
+  }
+};
+
 Blockly.Blocks['console_editline'] = {
   /**
    * Block to edit line in console.
