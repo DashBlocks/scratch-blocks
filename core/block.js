9672aa6ae8899eb93737961202b28489cb262907
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2026-06-06 23:45:29 +0500

    block.js: ⬆ Update

--- core/block.js
+++ core/block.js
@@ -1520,6 +1520,9 @@ Blockly.Block.prototype.interpolate_ = function(message, args, lastDummyAlign) {
       if (field) {
         fieldStack.push([field, element['name']]);
       } else if (input) {
+        if (element['shape']) {
+          input.setShape(element['shape']);
+        }
         if (element['check']) {
           input.setCheck(element['check']);
         }
